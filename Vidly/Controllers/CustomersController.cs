568c54fd2253b5b6045d675e2ccc0e3c63afe22f
James <james@tantami.co.uk>
2019-06-28 22:23:04 +0100

    End of lesson commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { id = 1, Name = "Jon Walsh"},
+                new Customer { id = 2, Name = "James Walsh"},
+                new Customer { id = 3, Name = "Mia Walsh"}
+            };
+
+            var viewResult = new CustomerViewModel
+            {
+                Customer = customers
+            };
+
+            return View(viewResult);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,21 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ActionResult Index()
+        {
+            var movie = new List<Movie>
+            {
+                new Movie { Name = "Shrek", Id = 1 },
+                new Movie { Name = "The Age of Adeline", Id = 2},
+                new Movie { Name = "Test", Id = 3}
+            };
+
+            var viewModel = new MovieViewModel
+            {
+                Movie = movie
+            };
+
+            return View(viewModel);
+        }
+
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            return View(movie);
-        }
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
 
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return View(viewModel);
         }
     }
 }
