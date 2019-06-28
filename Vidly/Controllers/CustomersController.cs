c5af9967fbfc4b21878d6fee94e4eac788fdf82b
James <james@tantami.co.uk>
2019-06-28 22:42:05 +0100

    End of exercise

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -26,5 +26,28 @@ namespace Vidly.Controllers
 
             return View(viewResult);
         }
+
+        [Route("customers/{id}")]
+        public ActionResult GetCustomer(int id)
+        {
+
+            id = id - 1;
+
+            var customers = new List<Customer>
+            {
+                new Customer { id = 1, Name = "Jon Walsh"},
+                new Customer { id = 2, Name = "James Walsh"},
+                new Customer { id = 3, Name = "Mia Walsh"}
+            };
+
+            var customer = new Customer
+            {
+                id = customers[id].id,
+                Name = customers[id].Name
+            };
+
+            return View(customer);
+
+        }
     }
 }
